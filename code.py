85f49fd5ff9a820a9696579db6c020e70930a1ea
idwesar <79699158+idwesar@users.noreply.github.com>
2021-10-03 10:47:30 +0100

    fixed the coordinates and the main loop

--- code.py
+++ code.py
@@ -2,85 +2,85 @@ import numpy as np
 import statistics as stats
 
 '''SETUP'''
-cols, rows = (10, 15) #n.b. coords are 0-9 by 0-14
+rows, cols = (15, 10) #n.b. coords are 0-9 by 0-14
 wires = [(7,3), (7,6)] #want the wires to be in the centre, should write a fucntion to calculate the points (hanfway down and a third and two thirds across respectively so that it can be scaled up more easily/i dont have to pick points)
-box = np.zeros((rows, cols)) #grid is 10 columns across by 15 rows down, its just confusing here
+box = np.zeros((rows, cols))
 
 box[wires[0][0]][wires[0][1]] = 10
 box[wires[1][0]][wires[1][1]] = -10
 
-print(box)
-
 '''FUNCTIONS'''
-def getSurround(col, row):
-    up = (col, row-1)
-    down = (col, row+1)
-    left = (col-1, row)
-    right = (col+1, row)
+def getSurround(row, col):
+    up = (row-1, col)
+    down = (row+1, col)
+    left = (row, col-1)
+    right = (row, col+1)
 
     return up, down, left, right
 
-def getCoordValue(coord, array):
-    value = array[coord[0]][coord[1]]
+def getCoordValue(row, col, array):
+    value = array[row][col]
 
     return value
 
-def getAverage(col, row, array):
-    surroundCoords = getSurround(col, row)
+def getAverage(row, col, array):
+    surroundCoords = getSurround(row, col)
     val = 0
     for item in surroundCoords:
-        val += getCoordValue(item, array)
+        val += getCoordValue(item[0], item[1], array)
 
     avg = val / 4
 
     return avg
 
-def checkNotEdge(coord, array):
-    if coord[0] == 0 or coord[0] == array.shape[0]-1:
+def checkNotEdge(row, col, array):
+    if row == 0 or row == array.shape[0]-1:
         return False
-    elif coord[1] == 0 or coord[1] == array.shape[1]-1:
+    elif col == 0 or col == array.shape[1]-1:
         return False
     else:
         return True
 
-def checkNotWire(coord, wirelist):
-    if (coord) == wirelist[0] or (coord) == wirelist[1]:
+def checkNotWire(row, col, wirelist):
+    if (row, col) == wirelist[0] or (row, col) == wirelist[1]:
         return False
     else:
         return True
+
 
 #TODO: make this an 'iterate' function when it works
 for row in range(rows):
     for col in range(cols):
-        if checkNotWire((col, row), wires):
-            if checkNotEdge((col, row), box):
-                print(col, row)
+        if checkNotWire(row, col, wires):
+            if checkNotEdge(row, col, box):
+                print(row, col)
+                avg = getAverage(row, col, box)
+                box[(row, col)] = round(avg, 2) # updates the original square with the average of its neighbours?
 
-                # avg = getAverage(col, row, box)
-                # box[(col,row)] = avg # updates the original square with the average of its neighbours?
+print(box)
 
 
 '''TESTING'''
 def testGetSurround():
     #arrange
     testarray = np.zeros((rows, cols))
-    col, row = 3, 2
+    testrow, testcol = 3, 2
 
     #act
-    surround = getSurround(col, row)
+    surround = getSurround(testrow, testcol)
 
     #assert
-    assert surround[0] == (3, 1)
-    assert surround[1] == (3, 3)
-    assert surround[2] == (2, 2)
-    assert surround[3] == (4, 2)
+    assert surround[0] == (2, 2)
+    assert surround[1] == (4, 2)
+    assert surround[2] == (3, 1)
+    assert surround[3] == (3, 3)
 
 def testCoordVal():
     #arrange
     testarray = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
 
     #act
-    val = getCoordValue((1, 1), testarray)
+    val = getCoordValue(1, 1, testarray)
 
     #assert
     assert val == 1
@@ -88,10 +88,10 @@ def testCoordVal():
 def testGetAverage():
     #arrange
     testarray = [0, 2, 0], [3, 0, 5], [0, 2, 0]
-    testcol, testrow = 1, 1
+    testrow, testcol = 1, 1
 
     #act
-    result = getAverage(testcol, testrow, testarray)
+    result = getAverage(testrow, testcol, testarray)
 
 
     #assert
@@ -100,14 +100,14 @@ def testGetAverage():
 def testNotEdgeZero():
     #arrange
     testarray = np.zeros((rows, cols))
-    xzero = (0, 10)
-    yzero = (8, 0)
-    bothzero = (0, 0)
+    xzrow, xzcol = 0, 10
+    yzrow, yzcol = 8, 0
+    bothzrow, bothzcol = 0, 0
 
     #act
-    xzresult = checkNotEdge(xzero, testarray)
-    yzresult = checkNotEdge(yzero, testarray)
-    bothresult = checkNotEdge(bothzero, testarray)
+    xzresult = checkNotEdge(xzrow,xzcol, testarray)
+    yzresult = checkNotEdge(yzrow, yzcol, testarray)
+    bothresult = checkNotEdge(bothzrow, bothzcol, testarray)
 
     #assert
     assert xzresult == False
@@ -117,12 +117,12 @@ def testNotEdgeZero():
 def testMiddle():
     #arrange
     testarray = np.zeros((rows, cols))
-    coord = (5, 7)
+    row, col = 5, 7
     wirelist = (1, 1), (8, 6)
 
     #act
-    edgetest = checkNotEdge(coord, testarray)
-    wiretest = checkNotWire(coord, wirelist)
+    edgetest = checkNotEdge(row, col, testarray)
+    wiretest = checkNotWire(row, col, wirelist)
 
     #assert
     assert edgetest == True
@@ -131,14 +131,14 @@ def testMiddle():
 def testNotEdgeMax():
     #arrange
     testarray = np.zeros((rows, cols))
-    xmax = (14, 2)
-    ymax = (1, 9)
-    bothmax = (14, 9)
+    xmaxrow, xmaxcol = 14, 2
+    ymaxrow, ymaxcol = 1, 9
+    bmaxrow, bmaxcol = 14, 9
     
     #act
-    xmresult = checkNotEdge(xmax, testarray)
-    ymresult = checkNotEdge(ymax, testarray)
-    bothresult = checkNotEdge(bothmax, testarray)
+    xmresult = checkNotEdge(xmaxrow, xmaxcol, testarray)
+    ymresult = checkNotEdge(ymaxrow, ymaxcol, testarray)
+    bothresult = checkNotEdge(bmaxrow, bmaxcol, testarray)
 
     #assert
     assert xmresult == False
@@ -151,7 +151,7 @@ def testNotWire():
     testwires = (3, 4), (2, 1)
 
     #act
-    result = checkNotWire((3, 4), testwires)
+    result = checkNotWire(3, 4, testwires)
     
 
     #assert
@@ -165,3 +165,4 @@ testNotEdgeMax()
 testNotWire()
 testMiddle()
 
+#TODO: put the relevant stuff into a main function pls.
